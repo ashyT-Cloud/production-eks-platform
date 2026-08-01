28360d789cad407466f785fcb4424a38baa06008
ashyT-Cloud <ashytech.innovate@gmail.com>
2026-08-01 10:12:00 +0000

    refactor(ci): allow multiple GitHub repositories for OIDC

--- terraform/modules/github-oidc/main.tf
+++ terraform/modules/github-oidc/main.tf
@@ -35,7 +35,8 @@ data "aws_iam_policy_document" "github_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_owner}/${var.github_repository}:*"
+        for repo in var.github_repositories :
+        "repo:${var.github_owner}/${repo}:*"
       ]
     }
 
